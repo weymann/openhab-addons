6670e1380afe9c1a783527b97cf7268ad02d447f
Bernd <bernd.weymann@gmail.com>
2020-06-15 18:14:05 +0200

    Revert "Ensure non null values for all update values"
    This reverts commit a8659717276115767fef3fdb6c9e45932caa4ca6.

--- bundles/org.openhab.binding.luftdateninfo/src/main/java/org/openhab/binding/luftdateninfo/internal/utils/NumberUtils.java
+++ bundles/org.openhab.binding.luftdateninfo/src/main/java/org/openhab/binding/luftdateninfo/internal/utils/NumberUtils.java
@@ -21,8 +21,6 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
  */
 @NonNullByDefault
 public class NumberUtils {
-    private final static Double UNDEF = new Double(-1);
-
     public static double round(Object o, int places) {
         double value = convert(o);
 
@@ -38,8 +36,7 @@ public class NumberUtils {
     }
 
     public static double convert(Object o) {
-        // ensure value not null
-        Double value = UNDEF;
+        Double value = null;
         if (o instanceof Integer) {
             value = (double) ((Integer) o).intValue();
         } else if (o instanceof String) {
